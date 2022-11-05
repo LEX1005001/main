7cb4aade88b508ae2f77d4133e239d97ec66ab00
LEX1005001 <AleX69.galkin@yandex.ru>
2022-11-05 19:57:08 +0300

    Задание 2

--- dz.py
+++ dz.py
@@ -1,6 +1,12 @@
 a=int(input("Введите длину: "))
-b=int(input("Введите высоту: "))
-for i in range(b):
-    for b in range(a):
-        print("*",end="")
+b=int(input("Введите ширину: "))
+for i in range(a):
+    if i == 0 or i == a - 1:
+        for j in range(b):
+            print('*', end=' ')
+    else:
+        print('*', end=' ')
+        for j in range(1, b - 1):
+            print(' ', end=' ')
+        print('*', end=' ')
     print()
